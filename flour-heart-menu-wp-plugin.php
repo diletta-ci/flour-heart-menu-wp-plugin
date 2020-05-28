7d4c30fbf4198ad2479bf2c8a29329ecf0a3dac5
GiodiCi <diletta@Dilettas-MacBook-Pro.local>
2020-05-28 17:22:25 +0200

    Plugin security check

--- flour-heart-menu-wp-plugin.php
+++ flour-heart-menu-wp-plugin.php
@@ -30,3 +30,5 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 Copyright 2005-2015 Automattic, Inc.
 */
+
+defined( 'ABSPATH' ) or die( 'Hey, what are you doing here? You silly human!' );
