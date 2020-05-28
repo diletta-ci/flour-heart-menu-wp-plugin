e1e2d6ec6fa19cb38a059a039ac219ef77b860e2
GiodiCi <diletta@Dilettas-MacBook-Pro.local>
2020-05-28 17:18:14 +0200

    Starting files

---
+++ flour-heart-menu-wp-plugin.php
@@ -0,0 +1,32 @@
+<?php 
+/**
+ * @package Flour heart menu wp plugin
+ */
+/*
+Plugin Name: Flour Heart Menu
+Plugin URI: https://github.com/giodi-ci/flour-heart-menu-wp-plugin
+Description: Basic plugin to add a menu for restaurant website.
+Version: 1.0.0
+Author: Giodi CI - Diletta Calá Impirotta
+Author URI: https://github.com/giodi-ci
+License: GPLv2 or later
+Text Domain: flour-heat-menu-wp-plugin  
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2005-2015 Automattic, Inc.
+*/
