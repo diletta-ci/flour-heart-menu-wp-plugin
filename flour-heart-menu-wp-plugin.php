2a02451ff9912c0351b87dfc5b28bd56c8e76553
GiodiCi <diletta@Dilettas-MacBook-Pro.local>
2020-06-02 17:19:57 +0200

    Plugin settings link and page

--- flour-heart-menu-wp-plugin.php
+++ flour-heart-menu-wp-plugin.php
@@ -36,9 +36,35 @@ defined( 'ABSPATH' ) or die( 'Hey, what are you doing here? You silly human!' );
 if ( !class_exists('FlourHeartMenuPlugin') ) {
 
     class FlourHeartMenuPlugin
-    {    
+    {   
+        public $plugin;
+
+        function __construct() {
+            $this->plugin = plugin_basename( __FILE__ );
+        }
+
         function register() {
             add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ));
+
+            add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+
+            add_filter( "plugin_action_links_$this->plugin", array( $this, 'settings_links' ) );
+        }
+
+        public function add_admin_pages() {
+            add_menu_page( 'Flour Heart Menu', 'Flour Heart Menu', 'manage_options', 'flour_heart_menu_plugin', array( $this, 'admin_index' ), 'dashicons-carrot', 110 );
+        }
+
+        public function admin_index() {
+            require_once plugin_dir_path( __FILE__ ) . 'templates/index.php';
+        }
+
+        public function settings_links( $links ) {
+            $settings_link = '<a href="admin.php?page=flour_heart_menu_plugin">Settings</a>';
+            
+            array_push( $links, $settings_link );
+
+            return $links;
         }
 
         function create_post_type() {
